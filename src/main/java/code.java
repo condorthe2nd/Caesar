3f2f5bc23ae604b472ce2174aa18154a7aa9ffb2
Yitzy Holtzberg <yitzyh18@gmail.com>
2021-05-02 21:16:04 -0400

    Initial commit

---
+++ src/main/java/code.java
@@ -0,0 +1,31 @@
+import java.nio.charset.StandardCharsets;
+import java.util.Scanner;
+
+public class code {
+    public static void main(String[] args) {
+
+
+        Scanner scanner = new Scanner(System.in);
+        char result;
+        char EncryptedChar;
+        int key = scanner.nextInt();
+        String encoded = scanner.next();
+        char[] end = new char[encoded.length()];
+        try {
+            for (int i = 0; i <= encoded.length()-1; i++) {
+                result = encoded.charAt(i);
+                if (result + key > 126) {
+                    EncryptedChar = (char) (32 + (result + key - (127)));
+                    end[i] = (EncryptedChar);
+                } else
+                    EncryptedChar = (char) (result + key);
+                end[i] = (EncryptedChar);
+            }
+        } catch (Exception e) {
+            System.out.println("Hello");
+        }
+        for (int i =0;i<end.length;i++){
+            System.out.print(end[i]);
+        }
+    }
+}
